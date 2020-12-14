3fa332b9fa66979ac96e6c8e028af770057d4b88
moritzebeling <git@moritzebeling.com>
2020-12-14 18:16:57 +0100

    box shadow for random images

--- src/layouts/random/Item.svelte
+++ src/layouts/random/Item.svelte
@@ -23,11 +23,12 @@
 
     article {
         position: absolute;
-        width: 15vw;
+        width: 20vw;
     }
     img {
         width: 100%;
         height: auto;
+        box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.3);
     }
 
 </style>
